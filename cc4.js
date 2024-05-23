5cb228eeb2fe0e573303d0274a754a241f3c0747
Irene Goff <ireneegoff@gmail.com>
2024-05-23 10:33:35 -0400

    Test EV class methods with different operations.

--- cc4.js
+++ cc4.js
@@ -33,3 +33,13 @@ class EV extends Car {
         this.charge -= 1;
         console.log(`${this.make} going at ${this.speed} km/h with a charge of ${this.charge}%`);
     }
+}
+
+// Test data: A 'Tesla' with starting speed 120 km/h and a battery charge of 23%
+const tesla = new EV('Tesla', 120, 23);
+
+// Tests:
+tesla.accelerate(); // Tesla going at 140 km/h with a charge of 22%
+tesla.brake(); // Tesla going at 135 km/h
+tesla.chargeBattery(50); // Tesla battery charged to 50%
+tesla.accelerate(); // Tesla going at 155 km/h with a charge of 49%
