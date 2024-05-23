68dd5d6fa29d589c05066db235500257558f6a51
Irene Goff <ireneegoff@gmail.com>
2024-05-23 10:31:15 -0400

    basic car class created

---
+++ cc4.js
@@ -0,0 +1,17 @@
+// Basic Car class:
+class Car {
+    constructor(make, speed) {
+        this.make = make;
+        this.speed = speed;
+    }
+
+    accelerate() {
+        this.speed += 10;
+        console.log(`${this.make} going at ${this.speed} km/h`);
+    }
+
+    brake() {
+        this.speed -= 5;
+        console.log(`${this.make} going at ${this.speed} km/h`);
+    }
+}
